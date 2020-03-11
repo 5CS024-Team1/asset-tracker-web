08e747bbc5190aa96833070cc4f4d04a4e655d06
Josh Shepherd <josh_lmao@live.co.uk>
2020-03-11 14:00:42 +0000

    Fixed container height on home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return (
-            <div className="h-100 w-100 d-flex">
+            <div className="w-100 d-flex container-full-width">
                 <div className="mx-auto my-5">
                     <h1 className="text-center" style={{ "font-size": "3rem" }} >Asset Angels</h1>
                     <h6 className="text-center">Tracking your assets so you don't have to <span role="img" aria-label="wink">😉</span></h6>
