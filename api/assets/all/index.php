3bc729f85afdc547d3839749c26cfd19dcb59d93
Matt Ward <matthewward00@outlook.com>
2020-04-03 18:38:46 +0100

    Added changes to all assets php

--- api/assets/all/index.php
+++ api/assets/all/index.php
@@ -37,22 +37,33 @@ if ($result->num_rows > 0)
     $db_array = array();
     while($row = $result->fetch_assoc()) {
         $asset = new Asset();
-        $asset->id = $row[$id];
-        $asset->display_name = $row[$display_name];
+        $asset->id = $row[$eqid];
+        $asset->barcode = $row[$barcode];
+        $asset->display_name = $row[$eqname];
         $asset->category = $row[$category];
         $asset->latitude = doubleval($row[$latitude]);
         $asset->longitude = doubleval($row[$longitude]);
         $asset->last_ping_time = $row[$last_ping_time];
-        $asset->barcode = $row[$barcode];
+        $asset->eqpatid = $row[$eqpatid];
+        $asset->date_loaned = $row[$loaned];
+        $asset->date_return = $row[$owner_date_return];
+        $asset->eqdept = $row[$eqdept];
+        $asset->last_cleaned = $row[$last_cleaned];
+
+        $sqlquery = "SELECT $surname, $forename, $personaddress, $personidspatient FROM $USER_TABLE WHERE $personidspatient = $eqid";
+        $sqlresult = $conn->query($sqlquery);
+        if ($sqlresult->num_rows > 0) 
+        {
+            while($row = $sqlresult->fetch_assoc()) {
+                $asset->surname = $row[$surname];
+                $asset->forename = $row[$forename];
+                $asset->personaddress = $row[$personaddress];
+                $asset->personidspatient = $row[$personidspatient];
+            }
+        }
 
         //$asset->purchase_cost;
         //$asset->origin = $row["origin"];
-        //$asset->owner_name = $row["owner_name"];
-        //$asset->owner_address = $row["owner_address"];
-        
-        $asset->date_loaned = $row[$loaned];
-        $asset->date_return = $row[$owner_date_return];
-        $asset->date_last_cleaned = $row[$last_cleaned];
 
         // Add asset to array
         $db_array[] = $asset;
--- api/resources/NHS_Tracker.sql
+++ api/resources/NHS_Tracker.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.3
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 13, 2020 at 12:13 PM
--- Server version: 5.7.24-0ubuntu0.16.04.1
--- PHP Version: 7.0.32-0ubuntu0.16.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2020 at 12:04 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,102 +19,111 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db1824591`
+-- Database: `nhs_asset_tracker`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Department`
+-- Table structure for table `department`
 --
 
-CREATE TABLE `Department` (
-  `PK_Dept_ID` int(3) NOT NULL,
-  `Name` varchar(15) NOT NULL,
-  `FLOOR` varchar(2) NOT NULL,
-  `Ward` varchar(3) NOT NULL
+CREATE TABLE `department` (
+  `Dept_ID` int(3) NOT NULL,
+  `Dept_Name` varchar(15) NOT NULL,
+  `Hosp_ID` varchar(10) NOT NULL,
+  `Hosp_Floor` varchar(2) NOT NULL,
+  `Hosp_Ward` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Equipment`
+-- Table structure for table `equipment`
 --
 
-CREATE TABLE `Equipment` (
-  `PK_ID` varchar(15) NOT NULL,
-  `Barcode` varchar(12) NOT NULL,
-  `Name` varchar(20) NOT NULL,
-  `Category` varchar(25) NOT NULL,
-  `Latittude` decimal(10,7) NOT NULL,
-  `Longitude` decimal(10,7) NOT NULL,
-  `Ping_Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `Loaned` datetime DEFAULT NULL,
-  `Return_due` datetime DEFAULT NULL,
-  `Last_Cleaned` datetime DEFAULT NULL
+CREATE TABLE `equipment` (
+  `Equi_ID` varchar(15) NOT NULL,
+  `Equi_Barcode` varchar(12) NOT NULL,
+  `Equi_Name` varchar(20) NOT NULL,
+  `Equi_Category` varchar(25) NOT NULL,
+  `Equi_Latittude` decimal(10,7) NOT NULL,
+  `Equi_Longitude` decimal(10,7) NOT NULL,
+  `Equi_Timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Equi_Assigned_Pats_IDs` int(10) DEFAULT NULL,
+  `Equi_Loaned` datetime DEFAULT NULL,
+  `Equi_Return_due` datetime DEFAULT NULL,
+  `Equi_Dept` int(3) DEFAULT NULL,
+  `Equi_Main_Last_Cleaned` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Hospital`
+-- Table structure for table `hospital`
 --
 
-CREATE TABLE `Hospital` (
-  `PK_Hosp_ID` int(10) NOT NULL,
-  `UN_Hospital_ID` varchar(10) NOT NULL,
-  `NAME` varchar(20) NOT NULL,
-  `Address` varchar(35) NOT NULL,
-  `Town` varchar(15) DEFAULT NULL,
-  `County` varchar(30) NOT NULL
+CREATE TABLE `hospital` (
+  `Hosp_ID` varchar(10) NOT NULL,
+  `Hosp_Name` varchar(20) NOT NULL,
+  `Hosp_Address` varchar(35) NOT NULL,
+  `Hosp_Town` varchar(15) DEFAULT NULL,
+  `Hosp_County` varchar(30) NOT NULL,
+  `Hosp_Floor` varchar(2) NOT NULL,
+  `Hosp_Ward` varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `IDs`
+-- Table structure for table `ids`
 --
 
-CREATE TABLE `IDs` (
-  `Patient_ID` varchar(10) NOT NULL,
-  `Staff_ID` varchar(10) NOT NULL
+CREATE TABLE `ids` (
+  `IDs_Patient` int(10) NOT NULL,
+  `IDs_Staff` varchar(10) NOT NULL,
+  `IDs_Inpatient` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Maintenance`
+-- Table structure for table `maintenance`
 --
 
-CREATE TABLE `Maintenance` (
+CREATE TABLE `maintenance` (
   `Issue #` varchar(2) NOT NULL,
   `Details` varchar(60) NOT NULL,
-  `Cleaning_Record` datetime NOT NULL
+  `Cleaning_Record` datetime NOT NULL,
+  `Hosp_ID` varchar(10) NOT NULL,
+  `Main_Last_Cleaned` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Person`
+-- Table structure for table `person`
 --
 
-CREATE TABLE `Person` (
-  `PK_Pats_ID` int(10) NOT NULL,
-  `Surname` varchar(15) NOT NULL,
-  `First_Name` varchar(15) NOT NULL,
-  `Address` varchar(25) NOT NULL,
-  `Town` varchar(15) DEFAULT NULL,
-  `County` varchar(30) NOT NULL
+CREATE TABLE `person` (
+  `Pers_Surname` varchar(15) NOT NULL,
+  `Pers_Forename` varchar(15) NOT NULL,
+  `Pers_Address` varchar(25) NOT NULL,
+  `Pers_Town` varchar(15) DEFAULT NULL,
+  `Pers_County` varchar(30) NOT NULL,
+  `IDs_Patient` int(10) NOT NULL,
+  `IDs_Staff` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `User`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `User` (
-  `Username` varchar(15) NOT NULL
+CREATE TABLE `user` (
+  `Username` varchar(15) NOT NULL,
+  `User_Pass` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -120,35 +131,87 @@ CREATE TABLE `User` (
 --
 
 --
--- Indexes for table `Department`
+-- Indexes for table `department`
 --
-ALTER TABLE `Department`
-  ADD PRIMARY KEY (`PK_Dept_ID`);
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`Dept_ID`),
+  ADD KEY `Dept_Hosp` (`Hosp_ID`);
 
 --
--- Indexes for table `Equipment`
+-- Indexes for table `equipment`
 --
-ALTER TABLE `Equipment`
-  ADD PRIMARY KEY (`PK_ID`);
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`Equi_ID`),
+  ADD KEY `Equi_Dept` (`Equi_Dept`),
+  ADD KEY `Equi_Main` (`Equi_Main_Last_Cleaned`),
+  ADD KEY `Equi_Assigned_Pats_IDs` (`Equi_Assigned_Pats_IDs`);
 
 --
--- Indexes for table `Hospital`
+-- Indexes for table `hospital`
 --
-ALTER TABLE `Hospital`
-  ADD PRIMARY KEY (`PK_Hosp_ID`),
-  ADD UNIQUE KEY `UN_Hospital_ID` (`UN_Hospital_ID`);
+ALTER TABLE `hospital`
+  ADD PRIMARY KEY (`Hosp_ID`);
 
 --
--- Indexes for table `IDs`
+-- Indexes for table `ids`
 --
-ALTER TABLE `IDs`
-  ADD PRIMARY KEY (`Staff_ID`);
+ALTER TABLE `ids`
+  ADD UNIQUE KEY `IDs_Staff` (`IDs_Staff`),
+  ADD UNIQUE KEY `IDs_Patient` (`IDs_Patient`);
 
 --
--- Indexes for table `Person`
+-- Indexes for table `maintenance`
 --
-ALTER TABLE `Person`
-  ADD PRIMARY KEY (`PK_Pats_ID`);
+ALTER TABLE `maintenance`
+  ADD PRIMARY KEY (`Cleaning_Record`),
+  ADD UNIQUE KEY `Main_Last_Cleaned` (`Main_Last_Cleaned`),
+  ADD KEY `Main_Hosp` (`Hosp_ID`);
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD UNIQUE KEY `IDs_Staff` (`IDs_Staff`),
+  ADD UNIQUE KEY `IDs_Patient` (`IDs_Patient`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD UNIQUE KEY `Username` (`Username`),
+  ADD UNIQUE KEY `User_Pass` (`User_Pass`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `department`
+--
+ALTER TABLE `department`
+  ADD CONSTRAINT `Dept_Hosp` FOREIGN KEY (`Hosp_ID`) REFERENCES `hospital` (`Hosp_ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `equipment`
+--
+ALTER TABLE `equipment`
+  ADD CONSTRAINT `Equi_Assigned_Pats_IDs` FOREIGN KEY (`Equi_Assigned_Pats_IDs`) REFERENCES `ids` (`IDs_Patient`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Equi_Dept` FOREIGN KEY (`Equi_Dept`) REFERENCES `department` (`Dept_ID`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Equi_Main` FOREIGN KEY (`Equi_Main_Last_Cleaned`) REFERENCES `maintenance` (`Main_Last_Cleaned`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD CONSTRAINT `Main_Hosp` FOREIGN KEY (`Hosp_ID`) REFERENCES `hospital` (`Hosp_ID`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `person`
+--
+ALTER TABLE `person`
+  ADD CONSTRAINT `Pers_Pats_ID` FOREIGN KEY (`IDs_Patient`) REFERENCES `ids` (`IDs_Patient`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `Pers_Staf_ID` FOREIGN KEY (`IDs_Staff`) REFERENCES `ids` (`IDs_Staff`) ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
