99d3473507a32b7372b936c40ffa2b949ca06f41
Matt Ward <matthewward00@outlook.com>
2020-04-28 18:25:14 +0100

    Created an auto increment to patient ID on ids table

---
+++ api/assets/get/patient-id/index.php
@@ -0,0 +1,57 @@
+<?php
+/// Entrypoint for getting the next available id number for when adding a new asset
+include_once("../../../api_config.php");
+include_once("../../../user/authentication.php");
+
+// Include cross origin headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header('Content-Type: application/json');
+
+// Check if request contains user auth
+if (!Authentication::requestContainsAuth($API_SECRET_KEY)) {
+    echo json_encode([
+        "patientId" => -1,
+        "error" => "Authorization token is required",
+    ], JSON_PRETTY_PRINT);
+    die();
+}
+
+// Use @ to suppress the warning and handle it ourselves
+@$conn = mysqli_connect($SERVER_LOCATION, $SERVER_USERNAME, $SERVER_PASSWORD, $DB_NAME);
+if(!$conn) 
+{
+    echo json_encode([
+        "patientId" => -1,
+        "error" => $e->getMessage(),
+    ], JSON_PRETTY_PRINT);
+    die();
+}
+
+/// Query to determine the next available asset id in table
+$sql = "SELECT $idspatient FROM $IDS_TABLE ORDER BY $idspatient DESC LIMIT 1";
+$result = $conn->query($sql);
+
+// Return determined free index
+if ($result && $result->num_rows > 0)
+{
+    $row = $result->fetch_assoc();
+    
+    // Get the last index and increment by one
+    $newId = $row[$eqid] + 1;
+    echo json_encode([
+        "patientId" => $newId,
+    ], JSON_PRETTY_PRINT);
+}
+else
+{
+    echo json_encode([
+        "patientId" => -1,
+        "error" => "Unable to find any data",
+    ], JSON_PRETTY_PRINT);
+}
+
+// Close connection when complete
+$conn->close();
+
+?>
